433077975fcad54c4a109281a15d8c8922b67ca5
cosemmanuel500 <31760850-cosemmanuel500@users.noreply.replit.com>
2024-02-01 14:52:56 +0000

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
-hhggh// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Emmanuel cos");
   }
 
   // @Test
